60a564bc29024c74fb9dcf96a35850506abab1ed
Speedwag00n <INemankov@luxoft.com>
2022-09-16 18:53:55 +0200

    Fix android autotests

--- jobs/Scripts/simpleRenderAndroid.py
+++ jobs/Scripts/simpleRenderAndroid.py
@@ -284,7 +284,7 @@ def execute_tests(args):
                         os.path.join(os.path.dirname(__file__),
                         "..",
                         "Configs",
-                        "settings_{}.json".format(getTransportProtocol(case).upper()))
+                        "settings.json")
                     ), 
                     settings_json_path
                 )
