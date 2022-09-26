4c593d9c923a991ddf5a10de1a0ca0fac4ca88c9
Speedwag00n <INemankov@luxoft.com>
2022-09-26 13:39:01 +0200

    Fix regions

--- jobs/Scripts/streaming_actions.py
+++ jobs/Scripts/streaming_actions.py
@@ -65,7 +65,7 @@ def set_dropdown_option(case, field_width, label_image_name, param_name):
 
 def configure_boolean_option(case, field_width, label_image_name, param_name):
     label_coords = utils.locate_on_screen(os.path.join(os.path.dirname(__file__), "..", "Elements", "AMDLink", f"{label_image_name}.png"))
-    region = (label_coords[0] + field_width, label_coords[1], field_width, label_coords[3])
+    region = (int(label_coords[0] + field_width), int(label_coords[1]), int(field_width), int(label_coords[3]))
     try:
         coords = utils.locate_on_screen(os.path.join(os.path.dirname(__file__), "..", "Elements", "AMDLink", f"enabled.png"), region=region)
         value = True
