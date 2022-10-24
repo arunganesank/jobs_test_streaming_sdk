e8e8243737200e37a864e47afb12fea898b9b94b
Speedwag00n <INemankov@luxoft.com>
2022-10-24 16:24:21 +0200

    Fix LoL opening

--- jobs/Scripts/server_actions.py
+++ jobs/Scripts/server_actions.py
@@ -130,8 +130,9 @@ class OpenGame(Action):
             if self.game_name == "lol":
                 sleep(240)
 
-            pyautogui.hotkey("win", "m")
-            sleep(0.5)
+            if platform.system() == "Windows":
+                pyautogui.hotkey("win", "m")
+                sleep(0.5)
 
             psutil.Popen(self.game_launcher, stdout=PIPE, stderr=PIPE, shell=True)
             self.logger.info("Executed: {}".format(self.game_launcher))
