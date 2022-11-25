4816e99db58efa70566c90ab88914447ab9bffc2
Speedwag00n <INemankov@luxoft.com>
2022-11-25 09:56:00 +0100

    Fix close_games script

--- autotests/scripts/close_games.py
+++ autotests/scripts/close_games.py
@@ -10,26 +10,26 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if game_name == "LoL":
+    if args.game_name == "LoL":
         subprocess.call("taskkill /f /im \"LeagueClient.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
         subprocess.call("taskkill /f /im \"League of Legends.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
-    elif game_name == "HeavenDX9" or game_name == "HeavenDX11" or game_name == "HeavenOpenGL":
+    elif args.game_name == "HeavenDX9" or args.game_name == "HeavenDX11" or args.game_name == "HeavenOpenGL":
         if platform.system() == "Windows":
             subprocess.call("taskkill /f /im \"browser_x86.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
             subprocess.call("taskkill /f /im \"Heaven.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
         else:
             subprocess.call("pkill \"browser_x64\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
             subprocess.call("pkill \"heaven_x64\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
-    elif game_name == "ValleyDX9" or game_name == "ValleyDX11" or game_name == "ValleyOpenGL":
+    elif args.game_name == "ValleyDX9" or args.game_name == "ValleyDX11" or args.game_name == "ValleyOpenGL":
         if platform.system() == "Windows":
             subprocess.call("taskkill /f /im \"browser_x86.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
             subprocess.call("taskkill /f /im \"Valley.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
         else:
             subprocess.call("pkill \"browser_x64\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
             subprocess.call("pkill \"valley_x64\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
-    elif game_name == "Dota2DX11" or game_name == "Dota2Vulkan":
+    elif args.game_name == "Dota2DX11" or args.game_name == "Dota2Vulkan":
         subprocess.call("taskkill /f /im \"dota2.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
-    elif game_name == "CSGO":
+    elif args.game_name == "CSGO":
         subprocess.call("taskkill /f /im \"csgo.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
-    elif game_name == "PUBG":
+    elif args.game_name == "PUBG":
         subprocess.call("taskkill /f /im \"TslGame.exe\"", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=30)
